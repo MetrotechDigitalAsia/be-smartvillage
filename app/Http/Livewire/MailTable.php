02859efb1ac1760f54559c8a71aee02c5a41b95d
Rahmat Riyadi <rahmat-riyadi@Rahmat-Riyadi-Macbook-Pro.local>
2023-05-28 17:17:24 +0800

    fix: mail inbox

--- app/Http/Livewire/MailTable.php
+++ app/Http/Livewire/MailTable.php
@@ -14,19 +14,7 @@ class MailTable extends Component
 
     public function mount(){
         $this->userDb = env('DB_RESIDENT_DATABASE'). '.resident_data';
-    }
-
-    public function render()
-    {
-        $this->data = $this->getData();
-        return view('livewire.mail-table');
-    }
-
-    public function getData(){
-
-        // return $this->data = 
-
-        return DB::table('users_mail as userMail')
+        $this->data = DB::table('users_mail as userMail')
         ->join('mails', 'mails.id', '=', 'userMail.mail_id')
         ->join('user_logins as user', function($join){
             $join->join($this->userDb.' as userDB', 'userDB.NIK', '=', 'user.no_nik')
@@ -47,6 +35,35 @@ class MailTable extends Component
             'userMail.created_at',
         ]);
     }
+
+    public function render()
+    {
+        return view('livewire.mail-table');
+    }
+
+    // public function getData(){
+
+    //     return DB::table('users_mail as userMail')
+    //     ->join('mails', 'mails.id', '=', 'userMail.mail_id')
+    //     ->join('user_logins as user', function($join){
+    //         $join->join($this->userDb.' as userDB', 'userDB.NIK', '=', 'user.no_nik')
+    //         ->on('user.id', '=', 'userMail.user_id');
+    //     })
+    //     ->where('userMail.status', 'Pending')
+    //     ->orderBy('userMail.created_at', 'ASC')
+    //     ->get([
+    //         DB::raw('ROW_NUMBER() OVER(ORDER BY userMail.id) as row_index'),
+    //         'userMail.id as id',
+    //         'userMail.mail_number',
+    //         'user.id as user_id',
+    //         'mails.id as mail_id',
+    //         'mails.title as mail_type',
+    //         'userDB.NAMA as name',
+    //         'userDB.NIK as nik',
+    //         'userMail.status as status',
+    //         'userMail.created_at',
+    //     ]);
+    // }
 
     public function changeStatus($id, $status){
 
