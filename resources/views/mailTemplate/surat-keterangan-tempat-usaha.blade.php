26c93a5de77d279a8875321090750bca4fdf9e10
Rahmat Riyadi <rahmat-riyadi@Rahmat-Riyadi-Macbook-Pro.local>
2023-06-30 14:44:40 +0800

    fix mi bede nabilang k pta

--- resources/views/mailTemplate/surat-keterangan-tempat-usaha.blade.php
+++ resources/views/mailTemplate/surat-keterangan-tempat-usaha.blade.php
@@ -12,15 +12,15 @@
     <table style="width: 100%; margin: 15px 0; margin-left: 16px;" >
         <tr>
             <td style="width: 200px; min-width: unset; max-width: unset;" >Nama</td>
-            <td>: {{ $data->applicant_name }}</td>
+            <td style="text-transform: capitalize;" >: {{ $data->applicant_name }}</td>
         </tr>
         <tr>
             <td>Jenis Kelamin</td>
-            <td>: {{ $data->applicant_sex }}</td>
+            <td style="text-transform: capitalize;" >: {{ $data->applicant_sex }}</td>
         </tr>
         <tr>
             <td>Tempat/Tgl.lahir</td>
-            <td>: {{ $data->applicant_birthplace }}, {{ Carbon\Carbon::parse($data->applicant_birthdate)->format('d-m-Y') }}</td>
+            <td style="text-transform: capitalize;" >: {{ $data->applicant_birthplace }}, {{ Carbon\Carbon::parse($data->applicant_birthdate)->format('d-m-Y') }}</td>
         </tr>
         <tr>
             <td>Kewarganegaraan</td>
@@ -28,7 +28,7 @@
         </tr>
         <tr>
             <td>Agama</td>
-            <td>: {{ $data->applicant_religion ?? '' }}</td>
+            <td style="text-transform: capitalize;" >: {{ $data->applicant_religion ?? '' }}</td>
         </tr>
         <tr>
             <td>Nomor KTP</td>
@@ -44,7 +44,7 @@
         </tr>
         <tr>
             <td>Nama Usaha</td>
-            <td>: {{ $field->business_name }}</td>
+            <td style="text-transform: capitalize;" >: {{ $field->business_name }}</td>
         </tr>
         <tr>
             <td>NPWP</td>
@@ -52,10 +52,10 @@
         </tr>
         <tr>
             <td>Alamat Usaha</td>
-            <td>: {{ $field->business_address }}</td>
+            <td style="text-transform: capitalize;" >: {{ $field->business_address }}</td>
         </tr>
     </table>
-    <p class="long" >Adalah salah satu warga desa kami yang bertempat tinggal pada alamat tersebut di atas dan berdasarkan pengetahuan kami hingga surat keterangan ini dikeluarkan memang benar yang bersangkutan memiliki usaha dibidang : “{{ $field->business_name }}”</p>
+    <p class="long" >Adalah salah satu warga desa kami yang bertempat tinggal pada alamat tersebut di atas dan berdasarkan pengetahuan kami hingga surat keterangan ini dikeluarkan memang benar yang bersangkutan memiliki usaha dibidang : “<span style="text-transform: capitalize;" >{{ $field->business_name }}</span>”</p>
     <p style="text-align: justify; text-indent: 40px; margin-top: 15px;" >Demikian surat keterangan usaha ini dibuat dengan sebenarnya untuk dapat dipergunakan sebagaimana mestinya.</p>
     @include('partials.mailFooter')
 </div>
