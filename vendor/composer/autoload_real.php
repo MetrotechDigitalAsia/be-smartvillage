1848887ba3f9a2fc155b46d7e2cec16a50e58f33
pimenlabs <pimenlabs@gmail.com>
2023-08-08 12:46:43 +0800

    update file

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,25 +33,18 @@ class ComposerAutoloaderInita43ccf00264847ce923c41beb4e872ce
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInita43ccf00264847ce923c41beb4e872ce::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequirea43ccf00264847ce923c41beb4e872ce($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInita43ccf00264847ce923c41beb4e872ce::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequirea43ccf00264847ce923c41beb4e872ce($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
