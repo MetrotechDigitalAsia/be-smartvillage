b1faff193e120f2fe80258275ef8600c026e44ee
Rahmat Riyadi <rahmat-riyadi@Rahmat-Riyadi-Macbook-Pro.local>
2023-08-09 10:01:09 +0800

    featL ui

--- app/Http/Controllers/API/Mobile/OutsiderController.php
+++ app/Http/Controllers/API/Mobile/OutsiderController.php
@@ -4,16 +4,18 @@ namespace App\Http\Controllers\API\Mobile;
 
 use App\Http\Controllers\API\ResponseController;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\OutsiderApiValidationRequest;
 use App\Models\Outsider;
+use Illuminate\Http\Request;
 
 class OutsiderController extends Controller
 {
     
-    public function store(OutsiderApiValidationRequest $outsiderApiValidationRequest){
+    public function store(Request $request){
+
+        $field = json_decode($request->field, true);
         
         try {
-            $data = Outsider::create($outsiderApiValidationRequest->all());
+            $data = Outsider::create($field);
             return ResponseController::create($data, 'success', 'create data successfully', 200);
         } catch (\Exception $exception){
             $message = $exception->getMessage();
