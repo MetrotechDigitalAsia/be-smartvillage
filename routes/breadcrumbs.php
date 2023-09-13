135e8ef5b0f567a03120cf4a1d94638c8dcce2f7
Rahmat Riyadi <rahmat-riyadi@Rahmat-Riyadi-Macbook-Pro.local>
2023-09-14 00:14:12 +0800

    feat: ui

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -20,6 +20,10 @@ use App\Models\UserData;
 use Diglactic\Breadcrumbs\Breadcrumbs;
 use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
 
+Breadcrumbs::for('dashboard', function (BreadcrumbTrail $trail): void {
+    $trail->push('', route('dashboard'));
+});
+
 Breadcrumbs::for('destinationPoint', function (BreadcrumbTrail $trail): void {
     $trail->push('Point Destinasi', route('destinationPoint'));
 });
