a6ab6c75c85aedc633ca6740af658aeaffa628b3
Rahmat Riyadi <rahmat-riyadi@Rahmat-Riyadi-Macbook-Pro.local>
2023-10-17 11:34:50 +0800

    feat: ui

--- resources/views/admin/mailView/tanda-tangan-suami-istri.blade.php
+++ resources/views/admin/mailView/tanda-tangan-suami-istri.blade.php
@@ -1,4 +1,4 @@
 <div style="height: 90vh;" >
-    <p style="text-align: center; margin: 170px 0 100px 0;" ><u>{{ $field->subject_1->name }}</u></p>
-    <p style="text-align: center;" ><u>{{ $field->subject_2->name }}</u></p>
+    <p style="text-align: center; margin: 170px 0 100px 0;" ><u>{{ $field->subject_1->name ?? '' }}</u></p>
+    <p style="text-align: center;" ><u>{{ $field->subject_2->name ?? '' }}</u></p>
 </div>
